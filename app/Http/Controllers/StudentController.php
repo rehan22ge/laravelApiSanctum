89e95b2f1aba73046dbfceed5e93162979883033
Rehan <zmrehan55@gmail.com>
2023-01-30 15:50:18 +0500

    Laravel Api CRUD Completed

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\student;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $student = student::all();
+        return $student;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'city' => 'required',
+            'fees' => 'required',
+        ]);
+
+        $student = student::create($request->all());
+        return $student;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $student = student::find($id);
+        return $student;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $student = student::find($id);
+        $student->update($request->all());
+        return $student;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $student = student::find($id)->delete();
+        return $student;
+    }
+
+    /**
+     * Search based City.
+     *
+     * @param  int  $city
+     * @return \Illuminate\Http\Response
+     */
+    public function search($city)
+    {
+        $student = student::where('city' , $city)->get();
+        return $student;
+    }
+
+}
